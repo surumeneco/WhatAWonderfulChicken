1bce199cb52008f70d8c4cc0e3c840b9e773b86a
surumeneco <128275841+surumeneco@users.noreply.github.com>
2026-09-25 03:32:59 +0900

    refactor: connect riding interaction controller

--- src/main/java/co/surumene/whatawonderfulchicken/listener/InteractionListener.java
+++ src/main/java/co/surumene/whatawonderfulchicken/listener/InteractionListener.java
@@ -8,6 +8,7 @@ import co.surumene.whatawonderfulchicken.display.DisplayService;
 import co.surumene.whatawonderfulchicken.gui.InventoryService;
 import co.surumene.whatawonderfulchicken.service.WonderfulChickenService;
 import co.surumene.whatawonderfulchicken.service.WonderfulChickenStore;
+import co.surumene.whatawonderfulchicken.task.RidingController;
 import co.surumene.whatawonderfulchicken.util.ItemUtil;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -40,9 +41,11 @@ public final class InteractionListener implements Listener {
     private final MessageService messages;
     private final InventoryService inventories;
     private final DisplayService displays;
+    private final RidingController riding;
 
     public InteractionListener(WhatAWonderfulChickenPlugin plugin, WonderfulChickenService chickens, WonderfulChickenStore store,
-                               ConfigService config, MessageService messages, InventoryService inventories, DisplayService displays) {
+                               ConfigService config, MessageService messages, InventoryService inventories, DisplayService displays,
+                               RidingController riding) {
         this.plugin = plugin;
         this.chickens = chickens;
         this.store = store;
@@ -50,6 +53,7 @@ public final class InteractionListener implements Listener {
         this.messages = messages;
         this.inventories = inventories;
         this.displays = displays;
+        this.riding = riding;
     }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
